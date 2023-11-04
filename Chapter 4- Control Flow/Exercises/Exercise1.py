c681b4e28af17ec779508a994c16f1e6774279c8
jeevanjoseph1 <147370893+jeevanjoseph1@users.noreply.github.com>
2023-11-04 14:21:10 +0400

    Exercise1.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise1.py
@@ -0,0 +1,9 @@
+# passing
+alien_color = 'green'
+if alien_color == 'green':
+    print("You just earned 5 points!")
+#failing
+alien_color = 'red'
+if alien_color == 'green':
+    print("You just earned 5 points!")
+
