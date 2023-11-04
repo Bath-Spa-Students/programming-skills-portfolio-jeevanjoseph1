82da9ec44ad0671bbea0a465b1bbfcaad0f6e787
jeevanjoseph1 <147370893+jeevanjoseph1@users.noreply.github.com>
2023-11-04 13:37:37 +0400

    Create Exercise2.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise2.py
@@ -0,0 +1,9 @@
+# print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+names=["joseph","jojo","uthram"]
+msg="hi,"+ names[0].title()+"!"
+print(msg)
+msg="hi,"+ names[1].title()+"."
+print(msg)
+msg="hi,"+ names[2].title()+"*"
+print(msg)
+
