a2559b85e5e11447dff4fbddde94949a3bba3f5c
jeevanjoseph1 <147370893+jeevanjoseph1@users.noreply.github.com>
2023-11-04 13:39:19 +0400

    Create Exercise3.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise3.py
@@ -0,0 +1,7 @@
+# Create a list of your favorite modes of transportation
+favorite_transportation = ["motorcycle", "car", "bicycle", "scooter", "bus"]
+print("i would like to buy a royal enfield", favorite_transportation[0])
+print("i would like to buy a", favorite_transportation[1])
+print("My favorite mode of transportation is my", favorite_transportation[2])
+print("the most fun transportation is my", favorite_transportation[3])
+print("My least favorite mode of transportation is the", favorite_transportation[4])
